43fb735a3018f2c23a5b816fcaf36c57e43fa220
Daniil <lm56dr50@gmail.com>
2024-08-17 00:03:43 +0300

    1

--- src/index.js
+++ src/index.js
@@ -36,9 +36,48 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
+/*function toStr(expr){
+    let map = new Map([
+        ['10', '.'], 
+        ['11', '-'],
+        ['00', MORSE_TABLE[str.substring(0,i)]],
+        ['**', ' ']
+      ]);
+    let str = '';
+    for (let i = 0; i < expr.length; i+=2){
+        let a =expr.substring(i,i+2);
+        str+=map.get(a);
+    }
+    return str;
+}*/
 function decode(expr) {
-    // write your solution here
+    let map = new Map([
+        ['10', '.'], 
+        ['11', '-'],
+        ['00', ''],
+        ['**', ' ']
+      ]);
+      let ans = '';
+      let str = '';
+      for (let i = 0; i < expr.length; i+=10){
+        let substr = expr.substring(i, i+10); 
+        if (substr == '**********') {
+          ans += ' ';
+          continue;
+        }
+        for (let j = 0; j < substr.length; j +=2){
+            let a = substr.substring(j,j+2);
+            if (a == '00') {
+                continue;
+            }
+            str += map.get(a);
+        }
+        ans += MORSE_TABLE[str];
+        str = '';
+      }
+
+
+    return ans;    
 }
 
 module.exports = {
